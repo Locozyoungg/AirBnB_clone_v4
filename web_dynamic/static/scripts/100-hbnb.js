f99544611534eb6242e6bdfe9ed699d6f28d010f
Locozyoungg <collaustine27@gmail.com>
2024-05-06 01:30:20 +0300

    100-hbnb

---
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    // Function to load places from the API
+    function loadPlaces() {
+        // Your AJAX request to load places from API
+    }
+
+    // Initial load of places
+    loadPlaces();
+
+    // Event listener for Apply Filters button click
+    $("#apply-filters").click(function() {
+        // Gather selected amenity IDs
+        var selectedAmenities = [];
+        $('input[type="checkbox"][name="amenity"]:checked').each(function() {
+            selectedAmenities.push($(this).data('id'));
+        });
+
+        // Gather selected state IDs
+        var selectedStates = [];
+        $('input[type="checkbox"][name="state"]:checked').each(function() {
+            selectedStates.push($(this).data('id'));
+        });
+
+        // Gather selected city IDs
+        var selectedCities = [];
+        $('input[type="checkbox"][name="city"]:checked').each(function() {
+            selectedCities.push($(this).data('id'));
+        });
+
+        // Make AJAX request to places_search endpoint with selected amenities, states, and cities
+        $.ajax({
+            type: "POST",
+            url: "http://0.0.0.0:5001/api/v1/places_search/",
+            contentType: "application/json",
+            data: JSON.stringify({
+                "amenities": selectedAmenities,
+                "states": selectedStates,
+                "cities": selectedCities
+            }),
+            success: function(data) {
+                // Clear existing places
+                $(".places").empty();
+
+                // Loop through places and create article tags
+                data.forEach(function(place) {
+                    var placeHTML = '<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + ' Guests</div><div class="number_rooms">' + place.number_rooms + ' Rooms</div><div class="number_bathrooms">' + place.number_bathrooms + ' Bathrooms</div></div><div class="description">' + place.description + '</div></article>';
+                    $(".places").append(placeHTML);
+                });
+            },
+            error: function(xhr, status, error) {
+                console.error("Error applying filters:", error);
+            }
+        });
+    });
+});
+
